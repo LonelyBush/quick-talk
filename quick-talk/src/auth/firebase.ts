ca12841b552304b2b4bf7044b4451d73a2aee7d0
LonelyBush <lonely.bush68@gmail.com>
2024-10-22 17:49:32 +0300

    feat: added initial styles and installed sass with css modules to the project

---
+++ quick-talk/src/auth/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOf1gzDXyUUudzGSkjwS1kPg260qbGlfA",
+  authDomain: "quick-talk-1732c.firebaseapp.com",
+  projectId: "quick-talk-1732c",
+  storageBucket: "quick-talk-1732c.appspot.com",
+  messagingSenderId: "488318031416",
+  appId: "1:488318031416:web:9945418232226d6e1f78cd",
+  measurementId: "G-MRRSKKS9E5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
